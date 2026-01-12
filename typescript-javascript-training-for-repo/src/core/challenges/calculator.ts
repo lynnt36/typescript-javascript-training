ab27b7f8f1e791877920cce7f5b4e2f14b4d665a
Lynn Thomas <Lynn.Thomas@DVSA.GOV.UK>
2026-01-12 13:05:17 +0000

    ammendments to calculator challenge

--- typescript-javascript-training-for-repo/src/core/challenges/calculator.ts
+++ typescript-javascript-training-for-repo/src/core/challenges/calculator.ts
@@ -4,13 +4,12 @@ const passMark = 50;
 
 // Function to check if the score is a pass
 const isPass = (score: number): boolean => {
-    // return true if score is 50 or above
+   
 };
 
 // Function to format the result message
 const getResultMessage = (score: number): string => {
     // use isPass(score)
-    // return a formatted string
 };
 
 const score = 72;
