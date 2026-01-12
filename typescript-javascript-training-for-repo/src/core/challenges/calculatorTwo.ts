7f9ced4ec99906063d8c406bf1f6df87be2b6b62
Lynn Thomas <Lynn.Thomas@DVSA.GOV.UK>
2026-01-12 13:44:35 +0000

    calculator challenge two

---
+++ typescript-javascript-training-for-repo/src/core/challenges/calculatorTwo.ts
@@ -0,0 +1,24 @@
+// Mini Project: Multi-Operation Calculator
+
+const num1 = 10;
+const num2 = 5;
+const operation = "add"; // add | subtract | multiply | divide
+
+const add = (a: number, b: number): number => {
+};
+
+const subtract = (a: number, b: number): number => {
+};
+
+const multiply = (a: number, b: number): number => {
+};
+
+const divide = (a: number, b: number): number => {
+};
+
+const calculate = (a: number, b: number, op: string): string => {
+    // decide which function to call
+    // return formatted string
+};
+
+// Log the result
