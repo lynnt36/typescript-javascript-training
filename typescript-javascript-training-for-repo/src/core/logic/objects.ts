384ddb9e66b91487aa184bbc59401da16663853c
Lynn Thomas <Lynn.Thomas@DVSA.GOV.UK>
2026-02-17 08:07:13 +0000

    added exercises to objects file

--- typescript-javascript-training-for-repo/src/core/logic/objects.ts
+++ typescript-javascript-training-for-repo/src/core/logic/objects.ts
@@ -43,6 +43,49 @@ class Objects {
         }
 
         console.log('All', all);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+
+        // BELOW ARE THE EXERCISES WE ATTEMPTED DURING LESSON 9 OF THE COURSE
+
+        // 1. Create an object called student with the following properties:
+        // - name
+        // - age
+        // - course
+        // Then log the entire object, the students name, and the students course.
+        // Goal: Understand object structure and property access.
+
+        // add code here
+
+        // 2. Create an object called car with:
+        // - brand
+        // - model
+        // - year
+        // Then Change the year to a newer value, Add a new property called color, and log the updated object.
+        // Goal: Understand that objects can be modified after creation.
+
+        // add code here
+
+        // 3. Create two objects
+        // - loginInfo with properties username and password
+        // - profileInfo with properties firstName, lastName and email
+        // Merge them into a new object called user.
+        // Log the merged object.
+        // Goal: Understand combining objects with the spread operator.
+
+        // add code here
+
+        // 4. Create an object called product with: name, price, stock
+        // Then: log all the keys, log all the values, use a loop to log each key.
+        // Goal: Understand Object.keys and working with object data dynamically.
+
+        // add code here
+
+        // 5. Create an object called employee with: name, position, address (which is another object with street, city, postcode)
+        // Then: destructure the city into a variable, and log the city.
+        // Goal: Understand nested objects and destructuring.
+
+        // add code here
     }
 }
 
